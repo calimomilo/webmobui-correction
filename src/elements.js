23763aaee4a7670bdcd4d6548aba1cc9ccceae8b
Camilo <paula@pena-parra.ch>
2026-02-20 17:56:01 +0100

    custom html element for song list

--- src/elements.js
+++ src/elements.js
@@ -19,4 +19,33 @@ class ArtistCover extends HTMLElement {
     }
 }
 
+class SongItem extends HTMLElement {
+    static observedAttributes = ["id, name"];
+
+    connectedCallback() {
+        this.render();
+    }
+
+    attributeChangedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.innerHTML =`
+        <a href="#">
+          <div class="list-item-title">${this.getAttribute("name")}</div>
+          <div class="list-item-actions">
+            <button type="button" class="icon-button favorite-button ">
+              <span class="material-icons">playlist_add</span>
+            </button>
+            <button type="button" class="icon-button play-button">
+              <span class="material-icons">play_arrow</span>
+            </button>
+          </div>
+        </a>
+        `
+    }
+}
+
 customElements.define('artist-cover', ArtistCover);
+customElements.define('song-item', SongItem);
--- src/songs.js
+++ src/songs.js
@@ -1,51 +1,58 @@
 import { getArtistSongs } from "./api.js";
 
-const createSongTitle = (song) => {
-    const title = document.createElement("div");
-    title.classList.add("list-item-title");
-    title.textContent = song.title;
-    return title;
-}
+// const createSongTitle = (song) => {
+//     const title = document.createElement("div");
+//     title.classList.add("list-item-title");
+//     title.textContent = song.title;
+//     return title;
+// }
 
-const createSongButton = (className, iconName) => {
-    const button = document.createElement("button");
-    button.type = "button";
-    button.classList.add("icon-button", className);
+// const createSongButton = (className, iconName) => {
+//     const button = document.createElement("button");
+//     button.type = "button";
+//     button.classList.add("icon-button", className);
 
-    const icon = document.createElement("span");
-    icon.classList.add("material-icons");
-    icon.textContent = iconName;
-    button.appendChild(icon);
+//     const icon = document.createElement("span");
+//     icon.classList.add("material-icons");
+//     icon.textContent = iconName;
+//     button.appendChild(icon);
 
-    return button;
-}
+//     return button;
+// }
 
-const createAddButton = () => {
-    return createSongButton("favorite-button", "playlist_add");
-}
+// const createAddButton = () => {
+//     return createSongButton("favorite-button", "playlist_add");
+// }
 
-const createPlayButton = () => {
-    return createSongButton("play-button", "play_arrow");
-}
+// const createPlayButton = () => {
+//     return createSongButton("play-button", "play_arrow");
+// }
 
-const createSongActions = () => {
-    const actions = document.createElement("div");
-    actions.classList.add("list-item-actions");
-    actions.appendChild(createAddButton());
-    actions.appendChild(createPlayButton());
-    return actions;
-}
+// const createSongActions = () => {
+//     const actions = document.createElement("div");
+//     actions.classList.add("list-item-actions");
+//     actions.appendChild(createAddButton());
+//     actions.appendChild(createPlayButton());
+//     return actions;
+// }
 
-const createSongElement = (song) => {
-    const element = document.createElement("a");
-    element.href = "#";
-    element.appendChild(createSongTitle(song));
-    element.appendChild(createSongActions());
-    return element;
-}
+// const createSongElement = (song) => {
+//     const element = document.createElement("a");
+//     element.href = "#";
+//     element.appendChild(createSongTitle(song));
+//     element.appendChild(createSongActions());
+//     return element;
+// }
+
+// const displaySong = (song, selector) => {
+//     const element = createSongElement(song);
+//     document.querySelector(selector).appendChild(element);
+// }
 
 const displaySong = (song, selector) => {
-    const element = createSongElement(song);
+    const element = document.createElement("song-item");
+    element.setAttribute("id", song.id);
+    element.setAttribute("name", song.title);
     document.querySelector(selector).appendChild(element);
 }
 
@@ -53,6 +60,7 @@ const loadArtistSongs = async (id) => {
     const songs = await getArtistSongs(id);
     document.querySelector("#list-section h4").textContent += songs[0].artist.name;
     songs.forEach(song => {
+        console.log(song);
         displaySong(song, ".list");
     })
 }
